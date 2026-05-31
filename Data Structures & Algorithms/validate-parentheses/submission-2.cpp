10a746090fa487b281e8b326d601e1d9beaa8b24
Hemansh Solanki <87089285+Hemanshs@users.noreply.github.com>
2026-05-31 16:21:51 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+bool matches(char t, char r)
+{
+    if(t=='(' && r==')')
+     return true;
+    else if(t=='[' && r==']')
+     return true;
+     else if(t=='{' && r=='}')
+     return true;
+     else 
+     return false;
+}
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(char c:s)
+        {
+            if(c=='('||c=='{'||c=='[')
+            {
+                st.push(c);
+
+            }
+            else 
+            { 
+                if(st.empty() || !matches(st.top(),c))
+                {
+                    return false;
+                }
+            st.pop();
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        else 
+        {
+            return false ; 
+        }
+    }
+};
